62a607a2a34bf208512961a27239668fd07a27db
JulianaRaS <jramirezsacarelo@gmail.com>
2023-06-23 20:00:24 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exam_juliana',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
